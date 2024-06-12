a17e991a60ec9780980624a50a6dc9add858c5b0
Aniket Patil <160469233+Aniket10720@users.noreply.github.com>
2024-06-12 18:27:03 +0530

    Create route_table.tf

---
+++ route_table.tf
@@ -0,0 +1,6 @@
+#aws_route_table_association 
+
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.some_public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
