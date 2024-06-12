08fa42a7fae92e386bc6f9f5b854fb9ddc0b72c5
Aniket Patil <160469233+Aniket10720@users.noreply.github.com>
2024-06-12 18:25:27 +0530

    Create internetgetway.tf

---
+++ internetgetway.tf
@@ -0,0 +1,8 @@
+#attaching aws internet getway using terraform 
+
+resource "aws_internet_gateway" "some_ig" {
+  vpc_id = aws_vpc.some_custom_vpc.id
+  tags = {
+    Name = "Some Internet Gateway"
+  }
+}
